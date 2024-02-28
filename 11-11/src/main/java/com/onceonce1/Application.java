23975b3a120409767e231b0aca999b22115e6cc4
Augusto Pavani <pavani.augusto@gmail.com>
2024-02-28 15:13:56 -0500

    chat server discord y boton de cargar foto de perfil

--- 11-11/src/main/java/com/onceonce1/Application.java
+++ 11-11/src/main/java/com/onceonce1/Application.java
@@ -1,13 +1,29 @@
 package com.onceonce1;
 
+import com.onceonce1.Controller.UsuarioController;
+import com.onceonce1.Entity.Usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		/*
+		UsuarioController userControl = new UsuarioController();
+		//crear un nuevo usuario
+		Usuario usuario1 = new Usuario(6,"John Doe", new Date(91, 5, 6), "johndoe", "password123", "123456789", "johndoe@example.com", "New York");
+
+		//guardar en la bas de datos
+		userControl.CrearUsuario(usuario1);
+
+		//crear una lista de usuarios para agregar
+		List<Usuario> listaUsuarios = new List<Usuario>();
+		*/
 	}
 
 }
